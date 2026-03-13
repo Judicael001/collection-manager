a7a5024f24d2dd3f1ce3188cf839d6ec07c883f4
Judicael001 <stjudicaelkoffi@gmail.com>
2026-03-13 16:46:18 +0000

    les components

---
+++ src/app/models/collection-item.ts
@@ -0,0 +1,9 @@
+export class CollectionItem {
+
+    name = "My Figurine";
+    description = "A legendary sword of unmatched sharpners and history.";
+    rarity = "Legendary";
+    price = 199;
+    image = "img/linx.png";
+    
+}
