9969222bc51af8ae633aaa85c80d6597c0f86994
Judicael001 <stjudicaelkoffi@gmail.com>
2026-03-23 19:08:28 +0000

    Les routes

--- src/app/pages/collection-item-detail/collection-item-detail.ts
+++ src/app/pages/collection-item-detail/collection-item-detail.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs'; // AJOUT : Importer Subscription
 
 @Component({
   selector: 'app-collection-item-detail',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './collection-item-detail.html',
   styleUrl: './collection-item-detail.scss',
 })
-export class CollectionItemDetail {}
+export class CollectionItemDetail {
+
+  private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+
+  itemId = input<number | null, string | null>(null, {
+    alias: 'id',
+    transform: value => value ? parseInt(value) : null
+  });
+}
